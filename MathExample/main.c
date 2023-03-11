82f16cb7059b76e3e55b664a86cd1817541e8590
Vensky <vovanvinni@gmail.com>
2023-03-11 15:22:43 +0300

    Initial Commit

---
+++ MathExample/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MathExample
+//
+//  Created by Vladimir Vinichenko on 11.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
